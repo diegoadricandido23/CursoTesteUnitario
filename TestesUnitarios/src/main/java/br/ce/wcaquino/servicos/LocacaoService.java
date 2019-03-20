e6203bbc792290069554f780f9d1d77a27db8dd2
Diego Adriano <diegoadricandido23@gmail.com>
2019-03-19 23:01:34 -0300

    Primeiro Teste

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -30,5 +31,17 @@ public class LocacaoService {
 
 	public static void main(String[] args) {
 		
+		//Cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		//acao
+		Locacao locacao =  locacaoService.alugarFilme(usuario, filme);
+		
+		//verificacao
+		System.out.println(locacao.getValor() == 5);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.adicionarDias(new Date(), 1)));
 	}
 }
